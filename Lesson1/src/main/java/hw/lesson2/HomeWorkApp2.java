40dd02a1579336a19881988998eda401be94d9cf
Maksim Vikarchuk <m.vikarchuk@xpert.ch>
2021-09-21 22:52:44 +0300

    les2

---
+++ Lesson1/src/main/java/hw/lesson2/HomeWorkApp2.java
@@ -0,0 +1,61 @@
+package hw.lesson2;
+
+import java.util.SimpleTimeZone;
+
+public class HomeWorkApp2 {
+
+    public static void main(String[] args) {
+
+/*        studySwitch();
+    }
+
+     private static void studySwitch() {
+        String nameLetter = "hhh";
+        switch (nameLetter) {
+            case "Olga":
+                System.out.println("Send letter to Olga");
+                break;
+            case "Boris":
+                System.out.println("Send letter to Boris");
+                break;
+            case "Maria":
+                System.out.println("Send letter to Maria");
+                break;
+            case "Alex":
+                System.out.println("Send letter to Alex");
+                break;
+            default:
+                System.out.println("Go to the office");
+                break;
+       }
+    }
+*/
+
+
+      /*  System.out.print("cat 1");
+        System.out.print("cat 2");
+        System.out.print("cat 3");
+        System.out.print("cat 4");
+        System.out.print("cat 5");
+        System.out.print("cat 6");
+        System.out.print("cat 7");
+        System.out.print("cat 8");
+        System.out.print("cat 9");
+        System.out.print("cat 10");
+
+       for (int i = 1; i <= 10; i++) {
+           System.out.print("Cat" + i + " ");
+       }
+*/
+
+
+       System.out.print("result:"());
+    }
+
+    public static int myMath(int value, int sign) {
+        int result = value;
+        for (int i = 1; i < sign; i++) {
+            result = result * value
+        }
+    }
+}
