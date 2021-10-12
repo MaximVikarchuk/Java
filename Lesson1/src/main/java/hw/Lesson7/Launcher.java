9b97fe324ff9123ab149484dd616406e06013b87
Maksim Vikarchuk <m.vikarchuk@xpert.ch>
2021-10-12 18:53:18 +0300

    les7

---
+++ Lesson1/src/main/java/hw/Lesson7/Launcher.java
@@ -0,0 +1,11 @@
+package hw.Lesson7;
+
+public class Launcher {
+
+    private static Object Window;
+
+    public static void main(String[] args) {
+        Window = new Window();
+
+    }
+}
---
+++ Lesson1/src/main/java/hw/Lesson7/Map.java
@@ -0,0 +1,13 @@
+package hw.Lesson7;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+
+    Map() {
+        setBackground(Color.BLACK);
+    }
+
+}
